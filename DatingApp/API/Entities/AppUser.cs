f9d917770b836e10cd1ab6292a31fc729bfff8cc
Hennie <henniesmit93@gmail.com>
2024-07-18 13:49:59 +0200

    End of Section 4

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -1,8 +1,12 @@
-﻿namespace API.Entities;
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace API.Entities;
 
 public class AppUser
 {
     //Properties
     public int Id { get; set; }
     public required string UserName { get; set;}
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
